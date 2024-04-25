8c8921d79a93909565aa2feff1915a50a2150ee5
Rivertripod3003 <166829264+Rivertripod3003@users.noreply.github.com>
2024-04-25 09:16:43 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print ("hello")
+responses= { "Hello" : "Hi there!",
+            "I lost my debit card" : "I've ordered a new one for you,
+            "I can't get into my account" : "You can reset your password under the Forgot Password section on the login page",
+            "" : "Hi there!",
+            "Hello" : "Hi there!",
+            "Hello" : "Hi there!",
+            "Hello" : "Hi there!",
+            }
+
